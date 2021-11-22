ab2ac7c93d5075a30f62954c9d0f2c76ae15fd4c
hedon954 <171725713@qq.com>
2021-11-22 14:16:48 +0800

    add: initial project with springboot framework.

---
+++ src/main/java/com/whu/gymnastics/GymnasticsApplication.java
@@ -0,0 +1,13 @@
+package com.whu.gymnastics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnasticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymnasticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/gymnastics/GymnasticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.gymnastics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnasticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
